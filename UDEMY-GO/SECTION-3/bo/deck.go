d360b257edcd8ab93acf48c83f062fdbf0251567
Saibal Ghosh <428145@cognizant.com>
2021-03-26 00:39:33 +0000

    NA

--- UDEMY-GO/SECTION-3/bo/deck.go
+++ UDEMY-GO/SECTION-3/bo/deck.go
@@ -1,19 +1,29 @@
 package bo
 
-
-type Deck []string;
-
+type Deck []string
 
 func CreateDeck() Deck {
 	cards := Deck{}
-	typeOfCards := []string{"SPADES","CLUBS","HEARTS","DUAMONDS"}
-	valOfCards  := []string{"ACE","QUEEN","KING"}
+	typeOfCards := []string{"SPADES", "CLUBS", "HEARTS", "DUAMONDS"}
+	valOfCards := []string{"ACE", "QUEEN", "KING"}
 
-	for _,item := range typeOfCards{
-		for _,item2 := range valOfCards{
-			cards = append(cards, item2+ " of "+item)
+	for _, item := range typeOfCards {
+		for _, item2 := range valOfCards {
+			cards = append(cards, item2+" of "+item)
 		}
 	}
 
 	return cards
+}
+
+func Deal(deck Deck, handSize int) (Deck, Deck) {
+	return deck[0:handSize], deck[handSize:]
+}
+
+func (deck Deck) ToString() string {
+	var concatenatedString string
+	for _, item := range deck {
+		concatenatedString = concatenatedString + item
+	}
+	return concatenatedString
 }
--- UDEMY-GO/SECTION-3/play.go
+++ UDEMY-GO/SECTION-3/play.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"io/ioutil"
 	bo "section3/bo"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	
-	deck  := bo.CreateDeck();
-	for _,card := range deck{
-		log.Info(card)
+
+	deck := bo.CreateDeck()
+
+	for _, card := range deck {
+		log.Info("DECK BEFORE SHUFFLE :: " + card)
 	}
+
+	deck, remainingDeck := bo.Deal(deck, 2)
+
+	for _, card := range deck {
+		log.Info("HAND :: " + card)
+	}
+
+	for _, card := range remainingDeck {
+		log.Info("REMAINING DECK :: " + card)
+	}
+	pointerToremainingDeck := &remainingDeck
+	log.Info(*pointerToremainingDeck)
+
+	//dataToWrite := []byte("Hello World!!!!")
+	ioutil.WriteFile("./deck.dat", []byte(remainingDeck.ToString()), 0644)
 
 }
